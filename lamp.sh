a402c54d74a32ae6d0c7f00c2fb322626705bd3f
irene-np <irene1smr1516@gmail.com>
2019-10-10 17:39:37 +0200

    a

--- lamp.sh
+++ lamp.sh
@@ -19,21 +19,28 @@ mv adminer-4.7.3-mysql.php index.php
 apt-get install debconf-utils -y
 
 # Configuramos la contraseña mysql
-debconf-set-selections <<< "mysql-sever mysql-server/root_password password $DB_ROOT_PASSWD=root"
-debconf-set-selections <<< "mysql-sever mysql-server/root_password_again password $DB_ROOT_PASSWD=root"
+DB_ROOT_PASSWD=root
+debconf-set-selections <<< "mysql-sever mysql-server/root_password password $DB_ROOT_PASSWD"
+debconf-set-selections <<< "mysql-sever mysql-server/root_password_again password $DB_ROOT_PASSWD"
  
 # Instalar mysql
 apt-get install mysql-server -y
 
 # Instalamos git
-apt-get install git
+apt-get install git -y
 
+# Instalamos la aplicacion
 cd /var/www/html
 git clone https://github.com/josejuansanchez/iaw-practica-lamp.git
-chown www-data:www-data iaw-irene-lamp/ -R
+chown www-data:www-data * -R
 
 # Configuramos la aplicacion web
 DB_NAME=lamp_db
 DB_USER=lamp_user
 DB_PASSWD=lamp_user
-mysql -u root -p$DB_ROOT_PASSWD <<< ""
+mysql -u root -p$DB_ROOT_PASSWD <<< "DROP DATABASE IF EXISTS $DB_NAME;"
+mysql -u root -p$DB_ROOT_PASSWD <<< "CREATE DATABASE $DB_NAME;"
+mysql -u root -p$DB_ROOT_PASSWD <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASSWD';"
+mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES"
+
+mysql -u root -p$DB_ROOT_PASSWD < /var/www/html/iaw-practica-lamp/db/database.sql
